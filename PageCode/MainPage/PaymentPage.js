ea474cd82b5db13360f14ad06b0cc7d67b471a1e
ash <91294460+ashthecoder05@users.noreply.github.com>
2024-09-24 16:22:07 +0100

    url website

--- PageCode/MainPage/PaymentPage.js
+++ PageCode/MainPage/PaymentPage.js
@@ -52,7 +52,7 @@ $w.onReady(function () {
 
     intervalId = setInterval(async () => {
       try {
-        const url = "https://aishwaryaadyanthay.wixsite.com/my-site/_functions/paymentstatus?addr=" + addresses;
+        const url = "https://"YOUR-SITE-URL"/my-site/_functions/paymentstatus?addr=" + addresses;
         const response = await fetch(url, { method: "get" });
         
         if (response.ok) {
